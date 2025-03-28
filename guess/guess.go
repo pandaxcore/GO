3e75d0828d21d738385ff6fa77a744b25cd7643c
pandaXcore <pandadesign7@gmail.com>
2025-03-28 23:08:55 +0500

    add loop

--- guess/guess.go
+++ guess/guess.go
@@ -11,24 +11,33 @@ import (
 )
 
 func main() {
-	target := rand.Intn(100)
+	target := rand.Intn(11)
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Make a guess: ")
-	input, err := reader.ReadString('\n')
-	var guess int
-	if err != nil {
-		log.Fatal(err)
-	}
-	input = strings.TrimSpace(input)
-	guess, err = strconv.Atoi(input)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if guess < target {
-		fmt.Println("Your guess was LOW.")
-	} else if guess > target {
-		fmt.Println("Your guess was HIGH")
-	}
 	fmt.Println(target)
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You got", 10-guesses, "left.")
+		fmt.Print("Make a guess: ")
+
+		input, err := reader.ReadString('\n')
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if guess < target {
+			fmt.Println("Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Your guess was HIGH")
+		} else {
+			fmt.Println("You GUESS is", guess)
+			return
+		}
+	}
 }
