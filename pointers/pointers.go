d8e1fe95944d19ba107370a4fa314c044e42bf46
pandaXcore <pandadesign7@gmail.com>
2025-03-31 00:13:08 +0500

    pointer in function

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+func createPointer() *float64 {
+	var myFloat = 98.5
+	return &myFloat
+}
+
 func main() {
 	var myInt int
 	var myIntPointer *int
 	myIntPointer = &myInt
 	*myIntPointer = 42
 	fmt.Println(myInt)
+
+	var myFloatPointer *float64 = createPointer()
+	fmt.Println(*myFloatPointer)
 }
