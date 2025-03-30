e8f3bc8c123134e5a1ad725c41b7f494da7c3728
pandaXcore <pandadesign7@gmail.com>
2025-03-30 23:53:24 +0500

    pointers, value\variable where in ramm it is keep I

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var myInt int = 4
-	var myIntPointer = &myInt
-	fmt.Println(myInt)
-	*myIntPointer = 8
-	fmt.Println(*myIntPointer)
+	var myInt int
+	var myIntPointer *int
+	myIntPointer = &myInt
+	*myIntPointer = 42
 	fmt.Println(myInt)
 }
