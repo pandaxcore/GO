1a1d81357d85ef648befb63a5e4797af41b33292
pandaXcore <pandadesign7@gmail.com>
2025-04-01 01:05:30 +0500

    pointers

--- negate_func/negate_func.go
+++ negate_func/negate_func.go
@@ -4,17 +4,29 @@ import (
 	"fmt"
 )
 
-func negate(myBoolean *bool) bool {
-	!myBoolean
-	return *myBoolean
+// func negate(myBoolean *bool) bool {
+// 	*myBoolean = !*myBoolean
+// 	return *myBoolean
+// }
+
+// func main() {
+// 	var truth bool = true
+// 	negate(&truth)
+// 	fmt.Println(truth)
+
+// 	var lies bool = false
+// 	negate(&lies)
+// 	fmt.Println(lies)
+// }
+
+func swapFunction(a *int, b *int) (int, int) {
+	*a, *b = *b, *a
+	return *a, *b
 }
 
 func main() {
-	var truth bool = true
-	negate(&truth)
-	fmt.Println(truth)
-
-	var lies bool = false
-	negate(&lies)
-	fmt.Println(lies)
+	var a, b int = 3, 7
+	fmt.Printf("Before Swap: a = %d, b = %d\n", a, b)
+	swapFunction(&a, &b)
+	fmt.Printf("After Swap: a = %d, b = %d\n", a, b)
 }
