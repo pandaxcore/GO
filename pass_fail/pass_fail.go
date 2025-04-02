ea7a46a6b1b9435c7d2ad9f111c4f8d7dd9ccf0c
pandaXcore <pandadesign7@gmail.com>
2025-04-02 09:23:26 +0500

    using an packages

--- pass_fail/pass_fail.go
+++ pass_fail/pass_fail.go
@@ -1,31 +1,55 @@
+// package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"strconv"
+// 	"strings"
+// )
+
+// func main() {
+// 	fmt.Print("Enter a grade: ")
+// 	reader := bufio.NewReader(os.Stdin)
+// 	input, err := reader.ReadString('\n')
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	input = strings.TrimSpace(input)
+// 	var grade float64
+// 	grade, err = strconv.ParseFloat(input, 64)
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	var status string
+// 	if grade >= 60 {
+// 		status = "passing"
+// 	} else {
+// 		status = "failing"
+// 	}
+// 	fmt.Println("A grade of", grade, "is", status)
+
+// }
+
 package main
 
 import (
-	"bufio"
+	"example/go/src/keyboard"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
 	fmt.Print("Enter a grade: ")
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-
+	grade, err := keyboard.GetFloat()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	input = strings.TrimSpace(input)
-	var grade float64
-	grade, err = strconv.ParseFloat(input, 64)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	var status string
 	if grade >= 60 {
 		status = "passing"
@@ -33,5 +57,4 @@ func main() {
 		status = "failing"
 	}
 	fmt.Println("A grade of", grade, "is", status)
-
 }
